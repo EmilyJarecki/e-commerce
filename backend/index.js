4a29588eaff6908245af550be85bf3894a53fca1
EmilyJarecki <jareckiemily@yahoo.com>
2023-01-30 18:04:33 -0600

    mongo and postman are connected. able to view product inputs from CRUD

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,13 @@
 const express = require("express");
 const app = express();
 
+
 //APP DEPENDENCIES
 const cors = require('cors')
 const morgan = require('morgan')
 
 // INITIALIZE .ENV VARIABLES
-require("dotenv").config("MONGODB_URI");
+require("dotenv").config();
 require("./config/db.connection")
 
 const { PORT, MONGODB_URI } = process.env;
@@ -23,9 +24,7 @@ app.use(express.json());
 app.use('/products', productController)
 app.use('/cart', cartController)
 
-app.get("/", (req, res) => {
-    res.redirect('/product');
-});
+
 
 // LISTENER
 app.listen(process.env.PORT || 4000, () => console.log(`listening on PORT ${PORT}`));
