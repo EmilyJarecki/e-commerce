75c98a5951f9a51e8b0da27e23370e2feed99418
EmilyJarecki <jareckiemily@yahoo.com>
2023-02-03 13:11:25 -0600

    for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const app = express();
 
-
 //APP DEPENDENCIES
 const cors = require('cors')
 const morgan = require('morgan')
@@ -17,17 +16,12 @@ const { PORT, MONGODB_URI } = process.env;
 const productController = require('./controllers/product-controller')
 const reviewController = require('./controllers/review-controller')
 
-
 //MIDDLEWARE
 app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json());
 app.use('/products', productController)
 app.use('/review', reviewController)
-
-
-
-
 
 // LISTENER
 app.listen(process.env.PORT || 4000, () => console.log(`listening on PORT ${PORT}`));
