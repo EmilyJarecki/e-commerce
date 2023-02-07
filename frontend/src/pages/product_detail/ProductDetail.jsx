8c105383c1148abd2b42f90ce5ed5b46e2e33463
EmilyJarecki <jareckiemily@yahoo.com>
2023-02-07 14:33:03 -0600

    can update only your own product

--- frontend/src/pages/product_detail/ProductDetail.jsx
+++ frontend/src/pages/product_detail/ProductDetail.jsx
@@ -51,6 +51,7 @@ const ProductDetail = () => {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
         },
         body: JSON.stringify(productData),
       });
