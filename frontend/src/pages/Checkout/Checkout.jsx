edaa0e1f5f14b6af2b0efd6fc8ba6d11a394d053
EmilyJarecki <jareckiemily@yahoo.com>
2023-05-02 09:38:06 -0500

    all information for items in checkout displayed

--- frontend/src/pages/Checkout/Checkout.jsx
+++ frontend/src/pages/Checkout/Checkout.jsx
@@ -1,15 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 const Checkout = () => {
   const [email, setEmail] = useState("");
+  const [cartData, setCartData] = useState([]);
+
   const checkValue = (e) => {
     const charCode = e.which ? e.which : e.keyCode;
     if (/\D/.test(String.fromCharCode(charCode))) {
       e.preventDefault();
     }
   };
+
+    // PRICE TOTAL
+    const getTotal = () => {
+      let total = 0;
+      cartData.forEach((product) => {
+        total += product.price * product.quantity;
+      });
+      return total.toFixed(2);
+    };
+
+    function getItemTotal(quantity, price) {
+      return quantity * price;
+    }
+
+  useEffect(() => {
+    const data = localStorage.getItem("cart");
+    const parsedData = JSON.parse(data);
+    if (parsedData !== null) {
+      setCartData(parsedData);
+    }
+  }, []);
 
   const states = [
     { name: "Alabama", abbr: "AL" },
@@ -249,6 +272,22 @@ const Checkout = () => {
             <button type="submit">Submit</button>
           </Form>
         </Formik>
+        <div className="">
+            {cartData.map((item) => (
+              <div className="" key={item._id}>
+                <div className="">
+                  <img className="cart-product-image" src={item.image} />
+                </div>
+
+                <p className="">{item.name}- Qty. {item.quantity}</p>
+                <p className="">Individual Item Cost: ${item.price.toFixed(2)}</p>
+                <p className="">Individual Cost x qty= ${getItemTotal(item.quantity, item.price).toFixed(2)}</p>
+
+              </div>
+            ))}
+            <div>-----------------------------------------------------</div>
+            <p className="">Total of Cart: ${getTotal()}</p>
+          </div>
       </div>
     </>
   );
