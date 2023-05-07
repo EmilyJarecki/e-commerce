638e076bf8a4bda1ca17c2ec714c564df11d4e7c
EmilyJarecki <jareckiemily@yahoo.com>
2023-05-06 21:05:13 -0500

    centered stuff

--- frontend/src/pages/Success/Success.jsx
+++ frontend/src/pages/Success/Success.jsx
@@ -20,27 +20,46 @@ const Success = () => {
   };
 
   return (
-    <div className="">
-      <div>
-        <h1  className="text-center ship-font text-4xl my-12">THANKS FOR SHOPPING WITH US!</h1>
+    <div className="flex flex-col">
+      <div className="">
+        <h1 className="text-center ship-font text-4xl my-12">
+          THANKS FOR SHOPPING WITH US!
+        </h1>
         <p className="ship-font text-xl text-center">
           We are getting started on your order right now, and you will receive
-          an order confirmation email shortly after to <span className="ship-font purple-link-color">{retrieveInfo()}</span>. If the
-          email hasn't arrived within two minutes, please check your spam folder
-          to see if the email was routed there.
+          an order confirmation email shortly after to{" "}
+          <span className="ship-font purple-link-color">{retrieveInfo()}</span>.
+          If the email hasn't arrived within two minutes, please check your spam
+          folder to see if the email was routed there.
         </p>
       </div>
-      <div>
-        <h3 className="ship-font text-3xl font-bold">Order #{randomNumber()}</h3>
-        <p className="ship-font text-sm">Expected Delivery: <span className="ship-font text-sm font-bold">{addSevenDays()}</span></p>
-      </div>
-      <Link  className="ship-font text-base font-medium button-class px-6 py-2 rounded-lg" to={`/shop`}>KEEP SHOPPING</Link>
-      <div className="bg-white">
-        <p className="ship-font text-xl">CONTACT US</p>
-        <p className="ship-font purple-link-color">1-800-123-4567</p>
-        <p className="ship-font purple-link-color">customerservice@aviato.com</p>
 
-        
+      <div className="h-32 self-center">
+        <p className="ship-font text-3xl font-bold text-center">Order #{randomNumber()}</p>
+        <p className="ship-font text-sm bg-white w-96 flex justify-between p-4 rounded ">
+          EXPECTED DELIVERY:{" "}
+          <span className="ship-font text-sm font-bold">{addSevenDays()}</span>
+        </p>
+      </div>
+      <div className="self-center">
+        <Link
+          className="ship-font text-base font-medium button-class px-6 py-2 rounded-lg"
+          to={`/shop`}
+        >
+          KEEP SHOPPING
+        </Link>
+      </div>
+
+      <div className="flex flex-col">
+        <div className="bg-white w-80 self-center">
+          <p className="ship-font text-xl text-center rounded">CONTACT US</p>
+          <p className="ship-font purple-link-color text-center">
+            1-800-123-4567
+          </p>
+          <p className="ship-font purple-link-color text-center">
+            customerservice@aviato.com
+          </p>
+        </div>
       </div>
     </div>
   );
